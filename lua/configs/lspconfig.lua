68eabb09b81ecadc680a0a2f58b3ff692c2086b6
JMcGuireCE <jpm31@email.sc.edu>
2025-06-01 14:19:49 -0400

    Better Python LSP and Harper

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "pylsp", "verible"}
+local servers = { "html", "cssls", "clangd","verible"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,6 +15,45 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+require('lspconfig').harper_ls.setup {
+  settings = {
+    ["harper-ls"] = {
+      userDictPath = "",
+      fileDictPath = "",
+      linters = {
+        SpellCheck = true,
+        SpelledNumbers = false,
+        AnA = true,
+        SentenceCapitalization = true,
+        UnclosedQuotes = true,
+        WrongQuotes = false,
+        LongSentences = true,
+        RepeatedWords = true,
+        Spaces = true,
+        Matcher = true,
+        CorrectNumberSuffix = true
+      },
+      codeActions = {
+        ForceStable = false
+      },
+      markdown = {
+        IgnoreLinkTitle = false
+      },
+      diagnosticSeverity = "hint",
+      isolateEnglish = false
+    }
+  }
+}
+
+lspconfig["basedpyright"].setup({
+  capabilities = capabilities,
+  settings = {
+    basedpyright = {
+      typeCheckingMode = "standard",
+    },
+  },
+})
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
